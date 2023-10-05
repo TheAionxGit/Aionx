c5401390bdfe34c40b6b051b35a2fdd1ce9da484
217Prototype <86744061+217Prototype@users.noreply.github.com>
2023-10-05 18:58:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author='Mikael Frenette',
     description='Library for designing Machine Learning models for time series forecast',
     packages=find_packages(),
+    py_modules=['Prototyping'],
     install_requires=[
         "numpy",
         "pandas==2.0.0",
