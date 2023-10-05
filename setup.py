e193e0efaff9b999ae446c35cc68bad211be8fee
217Prototype <86744061+217Prototype@users.noreply.github.com>
2023-10-05 17:14:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Thu Oct  5 16:49:46 2023
 from setuptools import setup, find_packages
 
 setup(
-    name='Prototyping',
+    name='prototyping',
     version='0.05',
     author='Mikael Frenette',
     description='Library for designing Machine Learning models for time series forecast',
