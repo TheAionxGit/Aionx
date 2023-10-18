e7988888e6fd8a0fb9a91468d245cc52e03d43da
Mikael Frenette <86744061+MikGitML@users.noreply.github.com>
2023-10-18 17:33:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  5 16:49:46 2023
-
-@author: User
-"""
-
 from setuptools import setup, find_packages
 
 setup(
-    name='Prototyping',
+    name='prototyping',
     version='0.05',
     author='Mikael Frenette',
     description='Library for designing Machine Learning models for time series forecast',
     packages=find_packages(),
-    py_modules=['Prototyping'],
+    py_modules=['prototyping'],
     install_requires=[
         "numpy",
         "pandas==2.0.0",
