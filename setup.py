359e859ba45f77095ec0ec3d13d55c4384b70164
Mikael Frenette <86744061+MikGitML@users.noreply.github.com>
2023-10-19 15:55:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='oikonometrica',
+    name='aion',
     version='1.0.0',
     author='Mikael Frenette',
     description='Library for designing Machine Learning models for time series forecast',
     packages=find_packages(),
-    py_modules=['oikonometrica'],
+    py_modules=['aion'],
     install_requires=[
         "numpy",
         "pandas==2.0.0",
